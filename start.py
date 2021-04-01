f6ee5f15949fc07994af2c51bab398b7fbc3cdd4
Ancieg <ancieg@bk.ru>
2021-04-01 09:29:13 +0300

    Updated start.py
    - changed Python version from 2 to 3
    - now gameoverlayrenderer.so library is searched at runtime

--- start.py
+++ start.py
@@ -1,17 +1,24 @@
-#!/usr/bin/env python2 
+#!/usr/bin/env python                                                           
 import os, sys
 from ctypes import CDLL
 
+# Steam Application Identifier for Morrowind                                    
 os.environ['SteamAppId'] = '22320'
 
-# Steam Overlay
-os.environ['LD_PRELOAD'] = os.getenv('HOME') + '/.local/share/Steam/ubuntu12_64/gameoverlayrenderer.so'
+# Steam Overlay                                                                 
+# It is made to make Steam track played time                                    
+home_dir = os.getenv('HOME')
+ubuntu12_64_dir = os.popen('find $HOME -name ubuntu12_64').read()
+overlay_path = ubuntu12_64_dir + '/gameoverlayrenderer.so'
+os.environ['LD_PRELOAD'] = home_dir + overlay_path
 
+# Load special library for Morrowind                                            
 steam_api = CDLL('./libsteam_api.so')
 try:
     steam_api.SteamAPI_Init()
 except:
-    print 'Failed to initialize Steam API'
+    print('Failed to initialize Steam API')
     sys.exit(1)
 
+# When all of preparations are ready, start openmw                              
 os.system('openmw')
